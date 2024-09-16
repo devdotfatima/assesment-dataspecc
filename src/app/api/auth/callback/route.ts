27035525bd213f39bbf91dfd9181f09720d0b08d
Fatima Ali <fatimaaliforwork@gmail.com>
2024-09-16 13:47:11 +0200

    adds try catch block

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,23 +2,36 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import TwitterService from "@/utils/twitterService";
 
-export const GET = async (request: NextRequest, res: NextResponse) => {
+export const GET = async (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
   const state = searchParams.get("state");
   const code = searchParams.get("code");
 
   if (!state || !code) {
-    return Response.json({ error: "Invalid code" }, { status: 400 });
+    return NextResponse.json(
+      { error: "Invalid state or code" },
+      { status: 400 }
+    );
   }
 
-  const twitterServiceInstance = TwitterService.getInstance();
+  try {
+    const twitterServiceInstance = TwitterService.getInstance();
+    const token = await twitterServiceInstance.requestAccessToken(code, state);
 
-  const token = await twitterServiceInstance.requestAccessToken(code, state);
-  console.log(token);
+    console.log(token);
 
-  cookies().set("token", JSON.stringify(token.token), {
-    httpOnly: true,
-    secure: true,
-  });
-  return NextResponse.redirect("/composepost");
+    const cookiesStore = cookies(); // Get cookies instance
+    cookiesStore.set("token", JSON.stringify(token.token), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Secure flag only in production
+    });
+
+    return NextResponse.redirect("/composepost");
+  } catch (error) {
+    console.error("Error requesting access token:", error);
+    return NextResponse.json(
+      { error: "Failed to get access token" },
+      { status: 500 }
+    );
+  }
 };
